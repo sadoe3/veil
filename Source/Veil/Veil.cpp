3bbbceaf6c2497dfbf92ef6f43b4e2f58f89bac2
Kyle <sadoe3@gmail.com>
2026-06-14 07:04:31 +0900

    initial project files

---
+++ Source/Veil/Veil.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Veil.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Veil, "Veil" );
+
+DEFINE_LOG_CATEGORY(LogVeil)
+ 
